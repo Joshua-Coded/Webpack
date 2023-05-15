669659dbd86963947f22f3d344e18ac25853c3cc
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-15 15:32:31 +0200

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,24 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, "src/index.js"),
+    entry:{ 
+    bundle: path.resolve(__dirname, "src/index.js"),
+   
+},
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js',
-    }
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test:/\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
 }
